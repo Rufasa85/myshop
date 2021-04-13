11687799dd9eb856e94a265384a934ef4946b277
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-04-12 17:05:07 -0700

    lesson one is done

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from "../../utils/actions";
+
 
 function CategoryMenu({ setCategory }) {
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+  const [state, dispatch] = useStoreContext();
 
+  const { categories } = state;
+  
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+useEffect(() => {
+  if(categoryData){
+    dispatch({
+      type:UPDATE_CATEGORIES,
+      categories:categoryData.categories
+    })
+  }
+}, [categoryData,dispatch])
   return (
     <div>
       <h2>Choose a Category:</h2>
@@ -13,7 +27,10 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            dispatch({
+              type:UPDATE_CURRENT_CATEGORY,
+              currentCategory:item._id
+            })
           }}
         >
           {item.name}
